bc06d2543d5e14c5596d485d5838959b8e8762b2
RYO <youbil.rachid@gmail.com>
2024-07-04 15:50:00 +0100

    corrections

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,9 @@
 import { Routes } from '@angular/router';
-
-import { TestComponent } from './test/test.component';
 import { CocktailListComponent } from './components/cocktail-list/cocktail-list.component';
 import { CocktailComponent } from './components/cocktail/cocktail.component';
 
 export const routes: Routes = [
-  { path: '', component: CocktailListComponent },
+  { path: 'cocktails', component: CocktailListComponent },
   { path: 'cocktail/:cocktailId', component: CocktailComponent },
+  { path: '**', redirectTo: 'cocktails' },
 ];
